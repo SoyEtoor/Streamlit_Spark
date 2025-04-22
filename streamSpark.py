c7823a60378fae0b7a5f144a5cba00d5eaf8e00d
Héctor Miguel <127361651+SoyEtoor@users.noreply.github.com>
2025-04-22 14:57:48 -0600

    Update streamSpark.py

--- streamSpark.py
+++ streamSpark.py
@@ -6,7 +6,7 @@ import requests
 CONFIG = {
     "JSONL_URL": "https://raw.githubusercontent.com/IngEnigma/StreamlitSpark/refs/heads/master/results/male_crimes/data.jsonl",
     "PRODUCER_URL": "https://kafka-postgres-producer-lyx5.onrender.com/send-crimes",
-    "PRODUCER_AREA_URL": "https://kafka-mongo-producer.onrender.com/send-areas",
+    "PRODUCER_AREA_URL": "https://kafka-mongo-producer-s73g.onrender.com/send-areas",
     "GITHUB_REPO_DEFAULT": "Streamlit_Spark",
     "GITHUB_USER_DEFAULT": "IngEnigma",
     "COLLECTION_NAME": "BigData",
